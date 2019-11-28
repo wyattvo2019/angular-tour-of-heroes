ff5c441e3f7388c3cfb83afa187299db70cc52c9
Wyatt <wyatt@enclave.vn>
2019-11-28 09:35:08 +0700

    List of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,6 +12,7 @@ export class HeroesComponent implements OnInit {
     id:1,
     name:'Windstorm'
   };
+  heroes = HEROES;
 
   constructor() { }
 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+    {id:11, name: 'Nice'},
+    {id:12, name: 'Good'},
+    {id:13, name: 'Veru Good'},
+    {id:14, name: 'Intelligent'},
+    {id:15, name: 'Alibaba'}
+]; 
